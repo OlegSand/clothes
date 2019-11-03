9dd47b822e735eaee943d59f377d6db203c4ec4a
OlegSand <oleg.sandyga@gmail.com>
2019-11-04 01:10:36 +0200

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const { watch, series,  src, dest, parallel } = require('gulp');
+const sass          = require('gulp-sass');
+const uglify        = require('gulp-uglify');
+const concat        = require('gulp-concat');
+const autoprefixer  = require('gulp-autoprefixer');
+const browserSync   = require('browser-sync').create();
+
+sass.compiler   = require('node-sass');
+
+function serve() {
+    browserSync.init({
+        server: "./app"
+    });
+
+    watch("app/scss/*.scss", sassFoo)
+    watch("app/*.html").on('change', browserSync.reload);
+}
+
+function sassFoo(cb) {
+    return src('dist/scss/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7' ], {cascade: true}))
+    .pipe(concat('style.css'))
+    .pipe(dest('app/css/'))
+    .pipe(browserSync.stream());
+}
+
+function compress() {
+    src('dist/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('all.js'))
+        dest('app/js')
+};
+
+function watcher() {
+    watch('dist/scss/**/*.scss', series(sassFoo));
+}
+
+
+
+exports.sassFoo = sassFoo;
+exports.compress = compress;
+exports.serve = serve;
+exports.watcher = watcher;
+exports.default = parallel(watcher, serve);
